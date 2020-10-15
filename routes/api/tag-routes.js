26f294656018faee4a209ac3a5a25d21eb6265ff
Marrione Nguyen <mtiamsic@gmail.com>
2020-10-15 13:19:06 -0700

    finished tag-route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -78,11 +78,12 @@ router.put('/:id', (req, res) => {
     {
       where: {
         id: req.params.id
-      },
-      {
-        tag_name: req.body.tag_name
       }
-    })
+    },
+    {
+      tag_name: req.body.tag_name
+    }
+  )
     .then(dbTagData => {
       if (!dbTagData) {
         res.status(404).json({ message: 'No category found with this id' });
@@ -98,6 +99,24 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy(
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+    .then(dbTagData => {
+      if (!dbTagData) {
+        res.status(404).json({ message: 'No category found with this id' });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 module.exports = router;
